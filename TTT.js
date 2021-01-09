0f91bb694684a71de4159ff54a982809ab391f76
Tamtox <39272747@protonmail.com>
2021-01-09 21:05:18 +0600

    2 players mode done

--- TTT.js
+++ TTT.js
@@ -4,15 +4,44 @@ const game = document.querySelector('#game');
 const board = document.querySelector('#board');
 const player1 = document.querySelector('#player-1');
 const player2 = document.querySelector('#player-2');
+const reset = document.querySelector('#reset');
+const mode = document.querySelector('#mode');
+const clear = document.querySelector('#clear');
 // Conditions and states
 const conditions = {
     turn:"X",
-    mode:"VS"
+    mode:"2 Players",
+    difficulty:"Easy"
 }
+// Buttons
+clear.addEventListener('click', function() {
+    functions.clearBoard();
+});
+reset.addEventListener('click',function() {
+    functions.resetBoard();
+});
+mode.addEventListener('click',function() {
+    functions.resetBoard();
+    functions.gameMode();
+});
 // Functions
 const functions = {
     parseBoard() {
-        
+        let  state = "";
+        let cells = document.querySelectorAll('.cell');
+        let count = 0;
+        // Convert board into string XXX-XXX-XXX
+        cells.forEach(x=>{
+            if(count ===3 || count === 6) {
+                state+="-"
+            }
+            let text = x.innerText;
+            if(text === "") {
+                state+=" ";
+            }
+            state+= text;
+            count+=1;
+        })
         if(/XXX|X...X...X|X....X....X|X..X..X/.test(state)) return 'X';
         if(/OOO|O...O...O|O....O....O|O..O..O/.test(state)) return 'O';
         if(state.includes(' ')) {
@@ -20,10 +49,26 @@ const functions = {
         }
         return "Draw";
     },
+    checkAndSet() {
+        if(conditions.mode === "2 Players") {
+            if(this.parseBoard() === "X") {
+                player1.children[1].innerText = parseInt(player1.children[1].innerText) + 1 + "";
+                this.clearBoard();
+            }
+            else if(this.parseBoard() === "O") {
+                player2.children[1].innerText = parseInt(player2.children[1].innerText) + 1 + "";
+                this.clearBoard();
+            }
+            else if(this.parseBoard() === "Draw") {
+                alert("Draw!");
+                this.clearBoard();
+            }
+        }
+    },
     generateBoard() {
         for(let i=0;i<9;i++) {
             let cell = document.createElement('div');
-            cell.innerText = " "
+            cell.innerText = " ";
             cell.classList.add('cell');
             // Conditional Borders
             if(i<=2) {
@@ -51,10 +96,21 @@ const functions = {
                         conditions.turn = "X"
                     }
                 }
+                // Check board
+                functions.checkAndSet();
             })
             board.appendChild(cell)
         }
     },
-
+    clearBoard() {
+        board.innerHTML = "";
+        this.generateBoard();
+        conditions.turn = "X";
+    },
+    resetBoard() {
+        this.clearBoard();
+        player1.children[1].innerText = '0';
+        player2.children[1].innerText = '0';
+    }
 }
 functions.generateBoard()
